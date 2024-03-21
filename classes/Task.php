5a7b42816941db1e550fff60f1e8aeb407de42bc
JonasDeBruyne <jonasdebruyne734@gmail.com>
2024-03-21 20:31:11 +0100

    new classe Task

---
+++ classes/Task.php
@@ -0,0 +1,80 @@
+<?php
+class Task
+{
+    private $label;
+    private $question;
+    private $answer;
+
+    /**
+     * Get the value of label
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * Set the value of label
+     *
+     * @return  self
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of question
+     */
+    public function getQuestion()
+    {
+        return $this->question;
+    }
+
+    /**
+     * Set the value of question
+     *
+     * @return  self
+     */
+    public function setQuestion($question)
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of answer
+     */
+    public function getAnswer()
+    {
+        return $this->answer;
+    }
+
+    /**
+     * Set the value of answer
+     *
+     * @return  self
+     */
+    public function setAnswer($answer)
+    {
+        $this->answer = $answer;
+
+        return $this;
+    }
+
+    public static function getTasks(PDO $pdo)
+    {
+        try {
+            $stmt = $pdo->prepare("SELECT * FROM roadmap");
+            $stmt->execute();
+            $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            return $tasks ?: [];
+        } catch (PDOException $e) {
+            error_log('Database error in getSubsidies(): ' . $e->getMessage());
+            throw new Exception('Database error: Unable to retrieve subsidies');
+        }
+    }
+}
