b6ebd255074418e95962c88875e3a0dda9f82c77
Stijn Houwelijckx <stijn@houwelijckx.be>
2024-06-18 15:35:20 +0200

    Added active task on dashboard

--- dashboard.php
+++ dashboard.php
@@ -4,6 +4,7 @@ include_once (__DIR__ . "/classes/User.php");
 include_once (__DIR__ . "/classes/Statute.php");
 include_once (__DIR__ . "/classes/Sector.php");
 include_once (__DIR__ . "/classes/Message.php");
+include_once (__DIR__ . "/classes/Task.php");
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('error_log', 'error.log');
@@ -24,6 +25,15 @@ if (isset($_SESSION["user_id"])) {
         // $pdo = Db::getInstance();
         $statutes = Statute::getAll($pdo);
         $sectors = Sector::getAll($pdo);
+
+        $activeTaskID = Task::getActiveTask($pdo, $_SESSION["user_id"], $user["statute_id"]);
+
+        if ($activeTaskID) {
+            $activeTask = Task::getTaskById($pdo, $activeTaskID["task_id"]);
+        } else {
+            $activeTask = null;
+        }
+
     } catch (Exception $e) {
         error_log('Database error: ' . $e->getMessage());
     }
@@ -48,8 +58,8 @@ if (isset($_SESSION["user_id"])) {
 <body>
     <?php include_once ('inc/nav.inc.php'); ?>
     <div id="dashboard">
-        <h1>Mijn dashboard</h1>
-        <div class="notifications">
+        <h1><?php echo $user["firstname"]?>'s dashboard</h1>
+        <!-- <div class="notifications">
             <h2>Meldingen</h2>
             <div class="notification">
                 <p>U heeft 3 ongelezen berichten in ‘Chat’.</p>
@@ -59,7 +69,51 @@ if (isset($_SESSION["user_id"])) {
                 <p>U heeft 1 opmerking bij het aanvragen van uw statuut als zelfstandige.</p>
                 <i class="fa fa-angle-right"></i>
             </div>
-        </div>
+        </div> -->
+        <?php if($activeTask): ?>
+            <div class="tasks">
+                <div class="task">
+                    <p class="label">
+                        <?php /*echo htmlspecialchars($activeTask["label"]);*/ ?>
+                        Huidige stap in stappenplan
+                    </p>
+                    <div class="row">
+                        <p class="question">
+                            <?php echo htmlspecialchars($activeTask["question"]); ?>
+                        </p>
+        
+                            <i class="fa fa-angle-down" data-task="<?php echo $activeTask['id']; ?>"></i>
+                    </div>
+                    <p class="answer">
+                        <?php echo htmlspecialchars($activeTask["answer"]); ?>
+                    </p>
+                    <div class="row display">
+                        <a href="taskDetails.php?question=<?php echo urlencode($activeTask["question"]); ?>" class="readmore">Ga naar stap</a>
+                    </div>
+                </div>
+
+                <div class="roadmap-link-container">
+                    <i class="fa fa-arrow-right"></i>
+                    <a class="roadmap-link" href="roadmap.php">Ga naar stappenplan</a>
+                </div>
+            </div>
+        <?php else: ?>
+            <div class="tasks">
+                <div class="task">
+                    <p class="label">
+                        Geen actieve stap in stappenplan
+                    </p>
+                    <div class="row">
+                        <p class="question">
+                            U heeft alle stappen in het stappenplan doorlopen.
+                        </p>
+                    </div>
+                    <div class="row display">
+                        <a href="roadmap.php" class="readmore">Ga naar stappenplan</a>
+                    </div>
+                </div>
+            </div>
+        <?php endif; ?>
     </div>
     <?php include_once ('inc/chat.inc.php'); ?>
 </body>
